e3ff63c80b2b108a362fb0f6d84ebd26fbc3c54d
rency <you@email.com>
2018-12-11 09:56:25 +0800

    index.html

--- server/sqlMap.js
+++
@@ -1,7 +0,0 @@
-var sqlMap = {
-  addValue: 'INSERT INTO user(name, age) VALUES (?, ?)',
-  getValue: 'SELECT * FROM user',
-  setValue: 'UPDATE user SET name = ? WHERE id = ?'
-}
-
-module.exports = sqlMap
